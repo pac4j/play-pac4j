038b9ab95e1db880dfcdb33283fe27fe0049584c
Ilias Elkhalloufi <ilias.elkhalloufi@randstadgroep.nl>
2016-07-20 11:32:46 +0200

    - change @since attribute

--- src/main/java/org/pac4j/play/store/PlaySessionStore.java
+++ src/main/java/org/pac4j/play/store/PlaySessionStore.java
@@ -7,7 +7,7 @@ import org.pac4j.play.PlayWebContext;
  * To store data in session.
  * Extending the SessionStore is necessary for dependency injection to work.
  * @author Ilias Elkhalloufi and Wessel Bakker
- * @since 1.9.0
+ * @since 2.5.0
  */
 public interface PlaySessionStore extends SessionStore<PlayWebContext> {
 
