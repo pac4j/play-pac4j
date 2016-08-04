32d5509e4cbeb5e6b9bbe2727d22d5e6e31a82fe
entropicdrifter <entropicdrifter@users.noreply.github.com>
2016-08-04 12:28:07 -0400

    Asynchronous HttpActionAdapterWrapper
    Makes calls to the HttpActionAdapter truly non-blocking code, more idiomatic for Play.
    Should allow for greater throughput.

--- src/main/java/org/pac4j/play/engine/HttpActionAdapterWrapper.java
+++ src/main/java/org/pac4j/play/engine/HttpActionAdapterWrapper.java
@@ -3,9 +3,12 @@ package org.pac4j.play.engine;
 import org.pac4j.play.PlayWebContext;
 import org.pac4j.core.http.HttpActionAdapter;
 import play.mvc.Result;
+import play.libs.concurrent.HttpExecutionContext;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+
+import com.google.inject.Inject;
 
 /**
  * Wrapper of a <code>HttpActionAdapter&lt;Result,PlayWebContext&gt;</code>.
@@ -14,15 +17,19 @@ import java.util.concurrent.CompletionStage;
  * @since 2.3.0
  */
 public final class HttpActionAdapterWrapper implements HttpActionAdapter<CompletionStage<Result>, PlayWebContext> {
+	
+	private final HttpActionAdapter<Result, PlayWebContext> wrapped;
+	
+	@Inject
+    private HttpExecutionContext ec;
 
-    private final HttpActionAdapter<Result, PlayWebContext> wrapped;
-
-    public HttpActionAdapterWrapper(final HttpActionAdapter<Result, PlayWebContext> wrapped) {
+	public HttpActionAdapterWrapper(final HttpActionAdapter<Result, PlayWebContext> wrapped) {
         this.wrapped = wrapped;
-    }
+}
 
-    @Override
-    public CompletionStage<Result> adapt(final int code, final PlayWebContext context) {
-        return CompletableFuture.completedFuture(wrapped.adapt(code, context));
-    }
+	@Override
+	public CompletionStage<Result> adapt(final int code, PlayWebContext context) {
+		return CompletableFuture.supplyAsync(() -> wrapped.adapt(code, context), ec.current());
+	}
+
 }
