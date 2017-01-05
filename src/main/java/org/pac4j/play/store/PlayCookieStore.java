2b845837ad814c9aff77538c741fbaf611f59c5f
entropicdrifter <entropicdrifter@users.noreply.github.com>
2017-01-05 13:42:07 -0500

    Javadocs, clean constructor
    updated for according to the discussion in pac4j/play-pac4j#166
    
    It wasn't clear if I should remove the cache, so I haven't yet. Doing so upon request should be easy.

--- src/main/java/org/pac4j/play/store/PlayCookieStore.java
+++ src/main/java/org/pac4j/play/store/PlayCookieStore.java
@@ -14,101 +14,126 @@ import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
 import org.pac4j.jwt.profile.JwtGenerator;
 import org.pac4j.jwt.JwtClaims;
 import org.pac4j.jwt.config.encryption.SecretEncryptionConfiguration;
-import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
 import com.google.inject.Inject;
 
 import play.cache.CacheApi;
 import play.mvc.Http;
 
+/**
+ * The Cookie storage uses an encrypted JWT inside the Play Session cookie for
+ * storage, allowing for a stateless backend.
+ * 
+ * The optional cache uses Play's native CacheApi as a performance boost. If the
+ * cache is enabled, it still falls back to the JWT in cases where the two
+ * disagree.
+ * 
+ * @author Nolan Barth
+ * @since 2.6.2
+ */
 public class PlayCookieStore implements PlaySessionStore {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(PlayCookieStore.class);
 
-    private final static String SEPARATOR = "$";
+	private final static String SEPARATOR = "$";
 
-    // prefix for the cache
-    private String prefix = "";
+	// prefix for the cache
+	private String prefix = "";
 
-    // 1 hour = 3600 seconds
-    private int timeout = 3600;
-    
-    private static boolean useCache = false;
-    private static SecretSignatureConfiguration secretSignatureConfig;
-    private static SecretEncryptionConfiguration secretEncryptConfig;
-    private JwtAuthenticator jwtAuthenticator;
-    private JwtGenerator<CommonProfile> jwtGenerator;
-    private final CacheApi cache;
-    
+	// 1 hour = 3600 seconds
+	private int timeout = 3600;
+
+	private boolean useCache = false;
+	private JwtAuthenticator jwtAuthenticator;
+	private JwtGenerator<CommonProfile> jwtGenerator;
+	private final CacheApi cache;
+
 	@Inject
-	public PlayCookieStore(final CacheApi cache) {
+	public PlayCookieStore(final CacheApi cache, final SecretEncryptionConfiguration secretEncryptConfig) {
 		super();
-		this.jwtAuthenticator = new JwtAuthenticator(secretSignatureConfig, secretEncryptConfig);
-		this.jwtGenerator = new JwtGenerator<CommonProfile>(secretSignatureConfig, secretEncryptConfig);
+		this.jwtAuthenticator = new JwtAuthenticator();
+		this.jwtAuthenticator.addEncryptionConfiguration(secretEncryptConfig);
+		this.jwtGenerator = new JwtGenerator<CommonProfile>();
+		this.jwtGenerator.setEncryptionConfiguration(secretEncryptConfig);
 		this.cache = cache;
 	}
 
 	String getKey(final String sessionId, final String key) {
-        return prefix + SEPARATOR + sessionId + SEPARATOR + key;
-    }
+		return prefix + SEPARATOR + sessionId + SEPARATOR + key;
+	}
 
-    @Override
-    public String getOrCreateSessionId(final PlayWebContext context) {
-        final Http.Session session = context.getJavaSession();
-        // get current sessionId
-        String sessionId = session.get(Pac4jConstants.SESSION_ID);
-        logger.trace("retrieved sessionId: {}", sessionId);
-        // if null, generate a new one
-        if (sessionId == null) {
-            // generate id for session
-            sessionId = java.util.UUID.randomUUID().toString();
-            logger.debug("generated sessionId: {}", sessionId);
-            // and save it to session
-            session.put(Pac4jConstants.SESSION_ID, sessionId);
-        }
-        return sessionId;
-    }
-	
+	@Override
+	public String getOrCreateSessionId(final PlayWebContext context) {
+		final Http.Session session = context.getJavaSession();
+		// get current sessionId
+		String sessionId = session.get(Pac4jConstants.SESSION_ID);
+		logger.trace("retrieved sessionId: {}", sessionId);
+		// if null, generate a new one
+		if (sessionId == null) {
+			// generate id for session
+			sessionId = java.util.UUID.randomUUID().toString();
+			logger.debug("generated sessionId: {}", sessionId);
+			// and save it to session
+			session.put(Pac4jConstants.SESSION_ID, sessionId);
+		}
+		return sessionId;
+	}
+
 	private String getSessionToken(final PlayWebContext context, String sessionId) {
 		final Http.Session session = context.getJavaSession();
-        // get current sessionToken using sessionId
-        String sessionToken = session.get(sessionId);
-        logger.trace("retrieved sessionToken: {}", sessionToken);
-        return sessionToken;
+		// get current sessionToken using sessionId
+		String sessionToken = session.get(sessionId);
+		logger.trace("retrieved sessionToken: {}", sessionToken);
+		return sessionToken;
 	}
-	
+
 	private void setSessionToken(final PlayWebContext context, String sessionId, Map<String, Object> sessionMap) {
 		final Http.Session session = context.getJavaSession();
-		//save the session as a JWT, replacing the previous instance if necessary.
-		if(session.containsKey(sessionId))
+		// save the session as a JWT, replacing the previous instance if
+		// necessary.
+		if (session.containsKey(sessionId))
 			session.remove(sessionId);
 		final String sessionToken = jwtGenerator.generate(sessionMap);
 		session.put(sessionId, sessionToken);
-		if(useCache) {
-			cache.set(getKey(sessionId, "MapCache"), sessionMap);
-			cache.set(getKey(sessionId, "TokenCache"), sessionToken);
-		}
+		if (useCache)
+			updateCache(sessionId, sessionToken, sessionMap);
 	}
-	
-	private Map<String, Object> getOrCreateSessionMap(final PlayWebContext context, String sessionId){
+
+	private void updateCache(String sessionId, String sessionToken, Map<String, Object> sessionMap) {
+		cache.set(getKey(sessionId, "MapCache"), sessionMap);
+		cache.set(getKey(sessionId, "TokenCache"), sessionToken);
+	}
+
+	private Map<String, Object> getOrCreateSessionMap(final PlayWebContext context, String sessionId) {
 		String sessionToken = getSessionToken(context, sessionId);
-		//get the claims, and if it fails, generate the session map
+		// get the claims, and if it fails, generate the session map
 		Map<String, Object> sessionMap;
 		try {
-			if(useCache && sessionToken.contentEquals(cache.get(getKey(sessionId, "TokenCache")))) {
-				sessionMap = (Map<String, Object>) cache.getOrElse(getKey(sessionId, "MapCache"), () -> {
-					return jwtAuthenticator.validateTokenAndGetClaims(sessionToken);
-				});
-			}
-			else {
+			if (useCache)
+				sessionMap = getSessionMapWithCache(sessionId, sessionToken);
+			else
 				sessionMap = jwtAuthenticator.validateTokenAndGetClaims(sessionToken);
-			}
-		}
-		catch(TechnicalException|NullPointerException e) {
+		} catch (TechnicalException | NullPointerException e) {
 			sessionMap = new HashMap<String, Object>();
-			//Ensures the map will be correctly built into a profile during the 'validate' call wrapped by validateTokenAndGetClaims()
+			// Ensures the map will be correctly built into a profile during the
+			// 'validate' call wrapped by validateTokenAndGetClaims()
 			sessionMap.put(JwtClaims.SUBJECT, sessionId);
 		}
 		return sessionMap;
+	}
+
+	private Map<String, Object> getSessionMapWithCache(String sessionId, String sessionToken)
+			throws TechnicalException, NullPointerException {
+		String cachedToken = cache.get(getKey(sessionId, "TokenCache"));
+		// check to see if the cache is up to date with the current sessionToken
+		if (sessionToken.contentEquals(cachedToken)) {
+			// If the cache is up to date, try to use it, if the cached map is
+			// missing, decrypt the token.
+			return (Map<String, Object>) cache.getOrElse(getKey(sessionId, "MapCache"), () -> {
+				return jwtAuthenticator.validateTokenAndGetClaims(sessionToken);
+			});
+		} else {
+			return jwtAuthenticator.validateTokenAndGetClaims(sessionToken);
+		}
 	}
 
 	@Override
@@ -116,30 +141,33 @@ public class PlayCookieStore implements PlaySessionStore {
 		String sessionId = getOrCreateSessionId(context);
 		return getOrCreateSessionMap(context, sessionId).get(getKey(sessionId, key));
 	}
-	
+
 	@Override
 	public void set(final PlayWebContext context, final String key, final Object value) {
 		String sessionId = getOrCreateSessionId(context);
 		Map<String, Object> sessionMap = getOrCreateSessionMap(context, sessionId);
-		if(key.contentEquals(Pac4jConstants.USER_PROFILES)) {
-			if (value instanceof LinkedHashMap<?,?>) {
-				@SuppressWarnings("unchecked")
-				LinkedHashMap<String, CommonProfile> profiles = (LinkedHashMap<String, CommonProfile>)value;
-				profiles.forEach((name, profile) ->{
-					profile.clearSensitiveData();
-				});
-				sessionMap.put(getKey(sessionId,key), profiles);
-			}
-			else {
-				CommonProfile profile = ((CommonProfile)value);
-				profile.clearSensitiveData();
-				sessionMap.put(getKey(sessionId,key), profile);
-			}
-		}
-		else {
-			sessionMap.put(getKey(sessionId,key), value);
+		if (key.contentEquals(Pac4jConstants.USER_PROFILES)) {
+			Object clearedProfiles = clearUserProfiles(value);
+			sessionMap.put(getKey(sessionId, key), clearedProfiles);
+		} else {
+			sessionMap.put(getKey(sessionId, key), value);
 		}
 		setSessionToken(context, sessionId, sessionMap);
+	}
+
+	private Object clearUserProfiles(Object value) {
+		if (value instanceof LinkedHashMap<?, ?>) {
+			@SuppressWarnings("unchecked")
+			LinkedHashMap<String, CommonProfile> profiles = (LinkedHashMap<String, CommonProfile>) value;
+			profiles.forEach((name, profile) -> {
+				profile.clearSensitiveData();
+			});
+			return profiles;
+		} else {
+			CommonProfile profile = ((CommonProfile) value);
+			profile.clearSensitiveData();
+			return profile;
+		}
 	}
 
 	public String getPrefix() {
@@ -158,22 +186,6 @@ public class PlayCookieStore implements PlaySessionStore {
 		this.timeout = timeout;
 	}
 
-	public static SecretSignatureConfiguration getSecretSignatureConfig() {
-		return secretSignatureConfig;
-	}
-
-	public static void setSecretSignatureConfig(SecretSignatureConfiguration secretSignatureConfig) {
-		PlayCookieStore.secretSignatureConfig = secretSignatureConfig;
-	}
-
-	public static SecretEncryptionConfiguration getSecretEncryptConfig() {
-		return secretEncryptConfig;
-	}
-
-	public static void setSecretEncryptConfig(SecretEncryptionConfiguration secretEncryptConfig) {
-		PlayCookieStore.secretEncryptConfig = secretEncryptConfig;
-	}
-
 	public JwtAuthenticator getJwtAuthenticator() {
 		return jwtAuthenticator;
 	}
@@ -190,12 +202,12 @@ public class PlayCookieStore implements PlaySessionStore {
 		this.jwtGenerator = jwtGenerator;
 	}
 
-	public static boolean isUseCache() {
+	public boolean isUseCache() {
 		return useCache;
 	}
 
-	public static void setUseCache(boolean useCache) {
-		PlayCookieStore.useCache = useCache;
+	public void setUseCache(boolean useCache) {
+		this.useCache = useCache;
 	}
 
 }
