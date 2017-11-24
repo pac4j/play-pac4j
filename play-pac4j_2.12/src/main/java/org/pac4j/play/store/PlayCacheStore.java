4982c26047d20cc5ec5ba90d3442048fba32e961
leleuj <leleuj@gmail.com>
2017-11-24 19:13:22 +0100

    Support Scala 2.11 and Scala 2.12

---
+++ play-pac4j_2.12/src/main/java/org/pac4j/play/store/PlayCacheStore.java
@@ -0,0 +1,85 @@
+package org.pac4j.play.store;
+
+import javax.inject.Inject;
+import com.google.inject.Provider;
+import org.pac4j.core.exception.TechnicalException;
+import org.pac4j.core.store.AbstractStore;
+import org.pac4j.core.util.CommonHelper;
+import org.pac4j.core.util.JavaSerializationHelper;
+import play.cache.SyncCacheApi;
+
+import java.io.Serializable;
+
+/**
+ * Store using the Play Cache.
+ *
+ * @author Jerome Leleu
+ * @since 3.0.0
+ */
+public class PlayCacheStore<K, O> extends AbstractStore<K, O> {
+
+    public static final JavaSerializationHelper JAVA_SERIALIZATION_HELPER = new JavaSerializationHelper();
+
+    private final SyncCacheApi cache;
+    private final Provider<SyncCacheApi> cacheProvider;
+    private int timeout;
+
+    @Inject
+    public PlayCacheStore(final SyncCacheApi cacheApi) {
+        this.cacheProvider = null;
+        this.cache = cacheApi;
+    }
+
+    public PlayCacheStore(final Provider<SyncCacheApi> cacheProvider) {
+        this.cache = null;
+        this.cacheProvider = cacheProvider;
+    }
+
+    @Override
+    protected void internalInit() {
+        CommonHelper.assertTrue(this.timeout >= 0, "timeout must be greater than zero");
+        if (this.cache == null && this.cacheProvider == null) {
+            throw new TechnicalException("The cache and the cacheProvider must not be both null");
+        }
+    }
+
+    @Override
+    protected O internalGet(final K key) {
+        return getCache().get(computeKey(key));
+    }
+
+    @Override
+    protected void internalSet(final K key, final O value) {
+        getCache().set(computeKey(key), value, this.timeout);
+    }
+
+    @Override
+    protected void internalRemove(final K key) {
+        getCache().remove(computeKey(key));
+    }
+
+    protected String computeKey(final Object objKey) {
+        if (objKey instanceof String) {
+            return (String) objKey;
+        } else {
+            return JAVA_SERIALIZATION_HELPER.serializeToBase64((Serializable) objKey);
+        }
+    }
+
+    public SyncCacheApi getCache() {
+        return cache != null ? cache : cacheProvider.get();
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(final int timeout) {
+        this.timeout = timeout;
+    }
+
+    @Override
+    public String toString() {
+        return CommonHelper.toString(this.getClass(), "cache", getCache(), "timeout", timeout);
+    }
+}
---
+++ play-pac4j_2.12/src/test/java/org/pac4j/play/store/PlayCacheSessionStoreTests.java
@@ -0,0 +1,60 @@
+package org.pac4j.play.store;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pac4j.core.util.TestsConstants;
+import org.pac4j.play.PlayWebContext;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import play.cache.SyncCacheApi;
+import play.mvc.Http;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+/**
+ * Tests {@link PlayCacheSessionStore}.
+ *
+ * @author Jerome Leleu
+ * @since 2.3.0
+ */
+@RunWith(PowerMockRunner.class)
+public final class PlayCacheSessionStoreTests implements TestsConstants {
+
+    private PlayCacheSessionStore store;
+    private PlayWebContext context;
+    private SyncCacheApi cacheApiMock;
+
+    @Before
+    public void setUp() {
+        cacheApiMock = mock(SyncCacheApi.class);
+        store = new PlayCacheSessionStore(cacheApiMock);
+        final Http.Session session = mock(Http.Session.class);
+        context = mock(PlayWebContext.class);
+        when(context.getJavaSession()).thenReturn(session);
+    }
+
+    @Test
+    public void testKey() {
+        assertEquals("$id$key", store.getKey(ID, KEY));
+    }
+
+    @Test
+    public void testGetOrCreateSessionId() {
+        assertNotNull(store.getOrCreateSessionId(context));
+    }
+
+    @Test
+    @PrepareForTest(SyncCacheApi.class)
+    public void testGetSet() {
+        mockStatic(SyncCacheApi.class);
+        PowerMockito.when(cacheApiMock.get(any(String.class))).thenReturn(VALUE);
+        store.setPrefix(KEY);
+        store.set(context, KEY, VALUE);
+        assertEquals(VALUE, store.get(context, KEY));
+    }
+}
