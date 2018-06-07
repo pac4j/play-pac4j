b84b4a6d745e4aa06147010d301fc60f5f970646
vidmantas zemleris <vidma@kensu.io>
2018-06-07 15:59:52 +0300

    Use JavaSerializationHelper

--- shared/src/main/java/org/pac4j/play/store/PlayCookieStore.java
+++ shared/src/main/java/org/pac4j/play/store/PlayCookieStore.java
@@ -4,13 +4,13 @@ import com.google.inject.Inject;
 import org.pac4j.core.context.Pac4jConstants;
 import org.pac4j.core.context.session.SessionStore;
 import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.util.JavaSerializationHelper;
 import org.pac4j.play.PlayWebContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.mvc.Http;
 
-import java.io.*;
-import java.util.Base64;
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 
 /**
@@ -28,6 +28,8 @@ public class PlayCookieStore implements PlaySessionStore {
 
 	private final String session_key_prefix = "pac4j_";
 
+    public static final JavaSerializationHelper JAVA_SERIALIZATION_HELPER = new JavaSerializationHelper();
+
 	@Inject
 	public PlayCookieStore() {
 	}
@@ -41,17 +43,12 @@ public class PlayCookieStore implements PlaySessionStore {
 	public Object get(final PlayWebContext context, final String key) {
 		final Http.Session session = context.getJavaSession();
 		String sessionValue = session.get(session_key_prefix + key);
-		try {
-			if (sessionValue == null) {
-				return null;
-			} else {
-				return fromString(sessionValue);
-			}
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+        if (sessionValue == null) {
+            return null;
+        } else {
+            return JAVA_SERIALIZATION_HELPER.unserializeFromBase64(sessionValue);
+        }
+    }
 
 	@Override
 	public void set(final PlayWebContext context, final String key, final Object value) {
@@ -62,12 +59,8 @@ public class PlayCookieStore implements PlaySessionStore {
 		logger.debug("PlayCookieStore.set, key = {}, value = {}", key, clearedValue);
 		final Http.Session session = context.getJavaSession();
 
-		try {
-			session.put(session_key_prefix + key, toString((Serializable) clearedValue));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        session.put(session_key_prefix + key, JAVA_SERIALIZATION_HELPER.serializeToBase64((Serializable) clearedValue));
+    }
 
 	// FIXME: can we implement this?
 	@Override
@@ -106,26 +99,4 @@ public class PlayCookieStore implements PlaySessionStore {
 		}
 	}
 
-    /**
-     * Read the object from Base64 string.
-     * https://stackoverflow.com/questions/134492/how-to-serialize-an-object-into-a-string
-     */
-    private static Object fromString(String s) throws IOException, ClassNotFoundException {
-        byte[] data = Base64.getDecoder().decode(s);
-        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
-        Object o = ois.readObject();
-        ois.close();
-        return o;
-    }
-
-    /**
-     * Write the object to a Base64 string.
-     */
-    private static String toString(Serializable o) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(o);
-        oos.close();
-        return Base64.getEncoder().encodeToString(baos.toByteArray());
-    }
 }
