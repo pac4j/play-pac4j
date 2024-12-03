516ccc9a5a2ed29f5e18f81177d09bfa79d3fb11
Justin Reardon <me@jmreardon.com>
2024-12-03 09:04:52 -0500

    Fix matcher handling Play 2.9.x backport (#729)
    * Fix matcher handling
    
    - Precompile regex
    - coalesce adjacent rules so that matchers behave properly
    
    * use current jdk converters

--- shared/src/test/java/org/pac4j/play/filters/SecurityFilterTests.scala
+++ shared/src/test/java/org/pac4j/play/filters/SecurityFilterTests.scala
@@ -35,10 +35,10 @@ class SecurityFilterTests extends ScalaFutures with Results {
     val config: Configuration = new Configuration(ConfigFactory.load("config/security_filter.conf"))
 
     SecurityFilter.loadRules(config) shouldBe Seq(
-      Rule("/path_anonymous", Some(RuleData("AnonymousClient", null, null))),
-      Rule("/path_secure_1", Some(RuleData("client1,client2", null, null))),
-      Rule("/path_secure_3", Some(RuleData(null, "authorizer1,authorizer2", null))),
-      Rule("/path_secure_4", Some(RuleData("client1,client2", "authorizer1,authorizer2", "matcher1,matcher2")))
+      Rule("/path_anonymous", List(RuleData("AnonymousClient", null, null))),
+      Rule("/path_secure_1", List(RuleData("client1,client2", null, null))),
+      Rule("/path_secure_3", List(RuleData(null, "authorizer1,authorizer2", null))),
+      Rule("/path_secure_4", List(RuleData("client1,client2", "authorizer1,authorizer2", "matcher1,matcher2")))
     )
   }
 
@@ -69,18 +69,64 @@ class SecurityFilterTests extends ScalaFutures with Results {
       """.stripMargin
     )
 
-    def tryFilterApply(path: String): Future[Result] = {
+    status(tryFilterApply(securityFilter, "/path_secure", POST)) shouldBe 401
+    status(tryFilterApply(securityFilter, "/path_secure_2/any_path_", POST)) shouldBe 401
+
+    status(tryFilterApply(securityFilter, "/path_anonymous", POST)) shouldBe 200
+    status(tryFilterApply(securityFilter, "any/other/path", POST)) shouldBe 200
+  }
+
+  @Test
+  def testThatSecurityFilterBlocksUnauthorizedRequestsWithMatchers(): Unit = {
+    implicit val ec = scala.concurrent.ExecutionContext.global
+    implicit val as = ActorSystem("text-actor-system")
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+
+    val securityFilter = prepareSecurityFilter(
+      """
+        |pac4j.security.rules = [
+        |  {
+        |    "/path_secure" = {
+        |      "clients" = "AnonymousClient"
+        |      "authorizers" = "none"
+        |      "matchers" = "put"
+        |    }
+        |  },
+        |  {
+        |    "/path_secure" = {
+        |      "clients" = "AnonymousClient"
+        |      "authorizers" = "none"
+        |      "matchers" = "get"
+        |    }
+        |  }, {
+        |    "/path_secure" = {
+        |      clients = "client1"
+        |      "matchers" = "post"
+        |    }
+        |  }, {
+        |    "/path_secure" = {
+        |      "clients" = "AnonymousClient"
+        |    }
+        |  }, {
+        |    "/path_secure/deeper" = {
+        |      clients = "client1"
+        |    }
+        |  }
+        |]
+      """.stripMargin
+    )
+
+    status(tryFilterApply(securityFilter, "/path_secure", GET)) shouldBe 200
+    status(tryFilterApply(securityFilter, "/path_secure", POST)) shouldBe 401
+    status(tryFilterApply(securityFilter, "/path_secure/deeper", GET)) shouldBe 401
+    status(tryFilterApply(securityFilter, "/path_secure/deeper", POST)) shouldBe 401
+  }
+
+  private def tryFilterApply(securityFilter: SecurityFilter, path: String, method: String): Future[Result] = {
       val nextFilter = (_: RequestHeader) => Future.successful(Ok("ok"))
-      val testRequest: RequestHeader = FakeRequest(POST, path)
+      val testRequest: RequestHeader = FakeRequest(method, path)
       securityFilter.apply(nextFilter)(testRequest)
     }
-
-    status(tryFilterApply("/path_secure")) shouldBe 401
-    status(tryFilterApply("/path_secure_2/any_path_")) shouldBe 401
-
-    status(tryFilterApply("/path_anonymous")) shouldBe 200
-    status(tryFilterApply("any/other/path")) shouldBe 200
-  }
 
   private def prepareSecurityFilter(configString: String)
                                    (implicit ec: ExecutionContext, mat: Materializer): SecurityFilter = {
