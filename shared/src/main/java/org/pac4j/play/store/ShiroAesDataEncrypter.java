4eb4dc56ab9820a3f09e8eed1554cb74d636ffce
rahul MALAWADKAR <49741611+relaxedboi@users.noreply.github.com>
2025-07-25 12:07:21 +0530

    Update ShiroAesDataEncrypter.java

--- shared/src/main/java/org/pac4j/play/store/ShiroAesDataEncrypter.java
+++ shared/src/main/java/org/pac4j/play/store/ShiroAesDataEncrypter.java
@@ -1,50 +1,13 @@
 package org.pac4j.play.store;
 
-import org.apache.shiro.crypto.AesCipherService;
-import org.pac4j.core.util.CommonHelper;
-
-import java.security.SecureRandom;
-
 /**
  * A DataEncrypter based on the Shiro library and AES encryption.
  *
  * @author Jerome Leleu
  * @since 6.1.0
  */
-public class ShiroAesDataEncrypter implements DataEncrypter {
-
-    private static final SecureRandom random = new SecureRandom();
-
-    private AesCipherService aesCipherService = new AesCipherService();
-
-    private byte[] key;
-
-    public ShiroAesDataEncrypter(final byte[] key) {
-        CommonHelper.assertNotNull("key", key);
-        this.key = key.clone();
-    }
-
-    public ShiroAesDataEncrypter() {
-        byte bytes[] = new byte[16];
-        random.nextBytes(bytes);
-        this.key = bytes;
-    }
-
-    @Override
-    public byte[] decrypt(byte[] encryptedBytes) {
-        if (encryptedBytes == null) {
-            return null;
-        } else {
-            return aesCipherService.decrypt(encryptedBytes, key).getBytes();
-        }
-    }
-
-    @Override
-    public byte[] encrypt(byte[] rawBytes) {
-        if (rawBytes == null) {
-            return null;
-        } else {
-            return aesCipherService.encrypt(rawBytes, key).getBytes();
-        }
-    }
+@Deprecated
+public class ShiroAesDataEncrypter extends JdkAesDataEncrypter {
+    public ShiroAesDataEncrypter(byte[] key) { super(key); }
+    public ShiroAesDataEncrypter() { super(); }
 }
